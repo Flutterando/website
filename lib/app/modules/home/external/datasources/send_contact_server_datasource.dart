3765bd6f3a02e3ed927b4032043be65a77ce9166
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2021-04-02 22:03:15 -0300

    Refactoring SendContactDatasource removed try-catch

--- lib/app/modules/home/external/datasources/send_contact_server_datasource.dart
+++ lib/app/modules/home/external/datasources/send_contact_server_datasource.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutterando/app/modules/home/domain/entities/send_contact/result_contact.dart';
 import 'package:flutterando/app/modules/home/domain/errors/errors_send_contact.dart';
 import 'package:flutterando/app/modules/home/infra/datasources/send_contact_datasource.dart';
@@ -8,23 +7,16 @@ import 'package:flutterando/app/modules/home/infra/models/send_contact/contact_m
 class SendContactServerDatasource implements SendContactDatasource {
   final Dio dio;
   late Map<String, dynamic> envVars;
-  SendContactServerDatasource(this.dio, this.envVars) {
-    envVars = env;
-  }
+  SendContactServerDatasource(this.dio, this.envVars);
 
   @override
   Future<ResultContact> sendContact(ContactModel contact) async {
-    try {
-      await dio.get(envVars["urlSendContact"], queryParameters: contact.toMap());
+    var response = await dio.get(envVars["urlSendContact"], queryParameters: contact.toMap());
+    
+    if(response.statusCode == 201) {
       return ResultContact('O seu email foi enviado com sucesso!'); 
-    } on DioError catch(e) {
-      throw SendContactExternalError(
-        message: e.response?.statusMessage ?? "Eror ao enviar o Email!"
-      );
-    } catch(e) {
-      throw SendContactExternalError(
-        message: "O Email não foi enviado!"
-      );
+    } else {
+      throw SendContactExternalError(message: "Eror ao enviar o Email!");
     }
   }
 }
--- test/app/modules/home/external/datasources/send_contact_server_datasource_test.dart
+++ test/app/modules/home/external/datasources/send_contact_server_datasource_test.dart
@@ -1,5 +1,6 @@
 
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutterando/app/modules/home/domain/entities/send_contact/result_contact.dart';
 import 'package:flutterando/app/modules/home/domain/errors/errors_send_contact.dart';
@@ -16,29 +17,23 @@ main() {
   final envsVar = {"title":"production", "urlSendContact":"flutterand_test@flutterando.com.br"};
   final datasource = SendContactServerDatasource(dio, envsVar);
   
-  setUpAll(() {
+  setUpAll(() async {
+    await DotEnv.testLoad();
     _requestOptionsMock = RequestOptionsMock();
   });
 
   test('Should return a ResultContact', () async {
     var contact = ContactModel(name: "Flutterando", message:"Hi!", email: "flutterando@flutterando.com.br");
     when(() => dio.get(any(), queryParameters: contact.toMap()))
-        .thenAnswer((_) async => Response(data: {}, statusCode: 200, requestOptions: _requestOptionsMock));
+        .thenAnswer((_) async => Response(data: {}, statusCode: 201, requestOptions: _requestOptionsMock));
     
     final future = await datasource.sendContact(contact);
     expect(future, isA<ResultContact>());
   });
 
-  test('Should return a error if statusCode isn\'t 200', () async {
+  test('Should return a error if statusCode isn\'t 201', () async {
     var contact = ContactModel(name: "Flutterando", message:"Hi!", email: "flutterando@flutterando.com.br");
-    when(() => dio.get(any(), queryParameters: contact.toMap())).thenThrow(DioError(
-      requestOptions: _requestOptionsMock,
-      response: Response(
-        statusCode: 401,
-        data: {'message': 'Request Error'}, requestOptions: _requestOptionsMock,
-      ),
-    ));
-    
+    when(() => dio.get(any(), queryParameters: contact.toMap())).thenThrow(SendContactExternalError(message: "Eror ao enviar o Email!"));
     final future = datasource.sendContact(contact);
     expect(future, throwsA(isA<SendContactExternalError>()));
   });
