ea3cbf37615da6d3f5f5b078402c381692efe54f
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2021-05-03 13:42:27 -0300

    Small modifications on data

--- lib/app/modules/home/widgets/header/widgets/header_social_media.dart
+++ lib/app/modules/home/widgets/header/widgets/header_social_media.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutterando/app/data/followers_number.dart';
 import 'package:flutterando/app/modules/home/widgets/header/header_controller.dart';
 import 'package:flutterando/app/utils/icons/icons.dart';
 import 'package:flutterando/app/utils/screen/screen_size.dart';
-import 'package:flutterando/app/utils/state/screen_state.dart';
 import 'package:flutterando/app/utils/text_styles/text_styles.dart';
 import 'package:flutterando/app/utils/url_launcher/urls.dart';
 import 'package:localization/localization.dart';
@@ -34,63 +32,41 @@ class HeaderSocialMedia extends StatelessWidget {
             style: TextStyles.roboto(16, fontWeight: FontWeight.normal),
           ),
           SizedBox(height: 20),
-          Observer(
-            builder: (_) {
-              if (headerController.socialMediaState.state is LoadingState) {
-                return CircularProgressIndicator();
-              }
-              if (headerController.socialMediaState.state is ErrorState) {
-                return Column(
-                  children: [
-                    Icon(
-                      Icons.warning,
-                      color: Colors.red,
-                    ),
-                    Text(
-                      "Ocorreu algum erro no carregamento dos dados!",
-                      style: TextStyle(color: Colors.redAccent),
-                    ),
-                  ],
-                );
-              }
-              final instagramNumber = headerController.resultInstaFollowers;
-              return Container(
-                width: double.infinity,
-                child: Wrap(
-                  alignment: WrapAlignment.center,
-                  runSpacing: 30,
-                  spacing: 30,
-                  children: [
-                    socialMediaIcon(
-                      IconUrls.youtube,
-                      urlLink: LinksUrl.youtube,
-                      followersNumber: followersNumber['youtube'],
-                    ),
-                    socialMediaIcon(
-                      IconUrls.instagram,
-                      urlLink: LinksUrl.instagram,
-                      followersNumber:
-                          instagramNumber.followersNumber.toString(),
-                    ),
-                    socialMediaIcon(
-                      IconUrls.twitter,
-                      urlLink: LinksUrl.twitter,
-                      followersNumber: followersNumber['twitter'],
-                    ),
-                    socialMediaIcon(
-                      IconUrls.discord,
-                      urlLink: LinksUrl.discord,
-                      followersNumber: followersNumber['discord'],
-                    ),
-                    socialMediaIcon(
-                      IconUrls.telegram,
-                      urlLink: LinksUrl.telegram,
-                      followersNumber: followersNumber['telegram'],
-                    )
-                  ],
+          Container(
+            width: double.infinity,
+            child: Wrap(
+              alignment: WrapAlignment.center,
+              runSpacing: 30,
+              spacing: 30,
+              children: [
+                socialMediaIcon(
+                  IconUrls.youtube,
+                  urlLink: LinksUrl.youtube,
+                  followersNumber: followersNumber['youtube'],
                 ),
-              );
-            },
+                socialMediaIcon(
+                  IconUrls.instagram,
+                  urlLink: LinksUrl.instagram,
+                  followersNumber: followersNumber['instagram']
+                      // instagramNumber.followersNumber.toString(),
+                ),
+                socialMediaIcon(
+                  IconUrls.twitter,
+                  urlLink: LinksUrl.twitter,
+                  followersNumber: followersNumber['twitter'],
+                ),
+                socialMediaIcon(
+                  IconUrls.discord,
+                  urlLink: LinksUrl.discord,
+                  followersNumber: followersNumber['discord'],
+                ),
+                socialMediaIcon(
+                  IconUrls.telegram,
+                  urlLink: LinksUrl.telegram,
+                  followersNumber: followersNumber['telegram'],
+                )
+              ],
+            ),
           ),
         ],
       ),
