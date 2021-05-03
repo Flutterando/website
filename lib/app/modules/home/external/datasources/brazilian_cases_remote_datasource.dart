f1c4731e565674ab07d2c666a14ddb3541e24d1a
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2021-05-03 14:31:32 -0300

    Updating Data itens

--- lib/app/modules/home/external/datasources/brazilian_cases_remote_datasource.dart
+++ lib/app/modules/home/external/datasources/brazilian_cases_remote_datasource.dart
@@ -6,12 +6,11 @@ import 'package:flutterando/app/modules/home/infra/models/result_brazilian_cases
 
 class BrazilianCasesRemoteDatasource implements BrazilianCasesDatasource {
   final Dio dio;
-  // final BrazilianCasesData brazilianCases;
   BrazilianCasesRemoteDatasource(this.dio);
 
   @override
   Future<List<ResultBrazilianCasesModel>> getBrazilianCases() async {
-    var response = await dio.get('https://raw.githubusercontent.com/Flutterando/website/tree/main/data/brazilian_cases_data.json');
+    var response = await dio.get('https://raw.githubusercontent.com/Flutterando/website/main/data/brazilian_cases_data.json');
     if(response.statusCode == 200) {
       var jsonList = jsonDecode(response.data) as List;
       var listBrazilianCases = jsonList.map((e) => ResultBrazilianCasesModel.fromMap(e)).toList();
