7dbd80629c095e4da2323e1e1d598b09bba63f57
titoomf <titoomf@gmail.com>
2022-09-20 10:40:30 -0300

    chnage external service github

--- lib/app/modules/home/external/datasources/brazilian_cases_remote_datasource.dart
+++ lib/app/modules/home/external/datasources/brazilian_cases_remote_datasource.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
+
 import 'package:dio/dio.dart';
+
 import 'package:flutterando/app/modules/home/infra/datasources/brazilian_cases_datasource.dart';
 import 'package:flutterando/app/modules/home/infra/models/result_brazilian_cases_model.dart';
 
@@ -10,10 +13,18 @@ class BrazilianCasesRemoteDatasource implements BrazilianCasesDatasource {
 
   @override
   Future<List<ResultBrazilianCasesModel>> getBrazilianCases() async {
-    var response = await dio.get('https://raw.githubusercontent.com/Flutterando/website/main/data/brazilian_cases_data.json');
-    if(response.statusCode == 200) {
+    var url =
+        'https://raw.githubusercontent.com/Titoomf/website/features/website/data/coorganizers_data.json';
+    // 'https://raw.githubusercontent.com/Flutterando/website/main/data/coorganizers_data.json';
+    var response = await dio.get(url);
+    if (kDebugMode) {
+      url =
+          'https://raw.githubusercontent.com/Titoomf/website/features/website/data/coorganizers_data.json';
+    }
+    if (response.statusCode == 200) {
       var jsonList = jsonDecode(response.data) as List;
-      var listBrazilianCases = jsonList.map((e) => ResultBrazilianCasesModel.fromMap(e)).toList();
+      var listBrazilianCases =
+          jsonList.map((e) => ResultBrazilianCasesModel.fromMap(e)).toList();
       return listBrazilianCases;
     }
 
