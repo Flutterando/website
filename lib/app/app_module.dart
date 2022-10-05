fa34b90d70ef137772ac91196d2bd3fc7e18719c
João Fialho <62760700+Joao-Fialho@users.noreply.github.com>
2022-10-05 12:02:36 -0300

    fix: Alteraçoes augusto

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,9 +1,7 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutterando/app/app_widget.dart';
-import 'package:flutterando/app/modules/home/home_module.dart';
 import 'package:flutterando/app/utils/screen/screen_size.dart';
 import 'package:flutterando/app/utils/service/http_service.dart';
 import 'package:flutterando/app/utils/url_launcher/url_launcher.dart';
