a5b77297c6df081b2e0b979c56f99d33c3ba1692
João Fialho <62760700+Joao-Fialho@users.noreply.github.com>
2022-10-10 13:12:51 -0300

    fix: ordem dos videos do youtube colocadas em ordem cronologica

--- lib/app/modules/home/external/datasources/youtube_remote_datasource.dart
+++ lib/app/modules/home/external/datasources/youtube_remote_datasource.dart
@@ -10,13 +10,14 @@ class YoutubeRemoteDatasource implements YoutubeDatasource {
   YoutubeRemoteDatasource(this.dio);
   @override
   Future<List<ResultYoutubeModel>> getYoutube() async {
-    var url = 'https://raw.githubusercontent.com/Flutterando/website/main/data/youtube_data.json';
+    var url =
+        'https://raw.githubusercontent.com/Flutterando/website/main/data/youtube_data.json';
     var response = await dio.get(url);
 
     if (response.statusCode == 200) {
       var jsonList = jsonDecode(response.data) as List;
-      var listYoutube = jsonList.map((e) => ResultYoutubeModel.fromMap(e)).toList();
-      listYoutube.shuffle();
+      var listYoutube =
+          jsonList.map((e) => ResultYoutubeModel.fromMap(e)).toList();
       return listYoutube;
     }
 
